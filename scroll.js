02d04a948f800babbb896e3e6f2ce1493209ca3f
Stefan <korun_stefan@hotmail.com>
2015-04-07 15:15:13 +0200

    neso inicjijalno

--- scroll.js
+++ scroll.js
@@ -1,13 +1,23 @@
 $(function () {
-    var video = $('video');
+    var videoWrapper = $('.videoWrapper');
+    var videoElement = videoWrapper.find('video');
+    var frameScrollSize = 0.5;
 
-    video.on('loadedmetadata', function(e) {
-        console.log(e, this);
+    handleScroll = _.throttle(function (newLocation) {
+        console.log('handling scroll');
+        videoElement[0].currentTime = (newLocation * frameScrollSize) / 1000;
+    }, 90, {trailing: true});
+
+    videoElement.on('loadedmetadata', function (event) {
+        console.log(this);
+
+        // adding frameScroll size to compensate for scroll size
+        var videoDuration = (this.duration + frameScrollSize) * 1000;
+        videoWrapper.height(videoDuration / frameScrollSize);
+    });
+    $(window).scroll(function (event) {
+        //console.log($(this).height());
+        handleScroll(this.scrollY);
     });
 
-
-    var section = window.location.hash.split('#')[1] || 100;
-    setInterval(function () {
-        video[0].currentTime += section / 1000;
-    }, section)
 });
