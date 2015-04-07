016e3132efd24e0103f576d1be1e95e73f424977
Stefan <korun_stefan@hotmail.com>
2015-04-07 20:02:27 +0200

    scroll with easing from codepen

--- scroll.js
+++ scroll.js
@@ -1,30 +1,83 @@
 $(function () {
+    // ---- Values you can tweak: ----
+    var accelamount = 0.01; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
+    var bounceamount = 0.9; //value from 0 to 1 for how much backlash back and forth you want in the easing. 0 = no bounce whatsoever, 1 = lots and lots of bounce
+
     var videoWrapper = $('.videoWrapper');
     var videoElement = videoWrapper.find('video');
-    var frameScrollSize = 0.5;
 
-    handleScroll = _.throttle(function (newLocation) {
-        console.log('handling scroll');
-        videoElement[0].currentTime = (newLocation * frameScrollSize) / 1000;
-    }, 90, {trailing: true});
+    var pixelDuration = 3; // miliseconds per pixel
+    var targetPosition = 0;
+    var currentPosition = 0;
+    var accel = 0;
 
-    videoElement.on('loadedmetadata', function (event) {
-        console.log(this);
 
+    videoElement.on('loadedmetadata', function () {
+        // Initializing scroll size
         // adding frameScroll size to compensate for scroll size
-        var videoDuration = (this.duration + frameScrollSize) * 1000;
-        videoWrapper.height(videoDuration / frameScrollSize);
+        var videoDuration = (this.duration + pixelDuration) * 1000;
+        videoWrapper.height(videoDuration / pixelDuration);
     });
-    $(window).scroll(function (event) {
-        //console.log($(this).height());
-        handleScroll(this.scrollY);
+    $(window).scroll(function () {
+        targetPosition = this.scrollY;
     });
 
 
-    // testing supported intervals
-    /*var section = window.location.hash.split('#')[1] || 100;
     setInterval(function () {
-        video[0].currentTime += section / 1000;
-    }, section)*/
+
+        //Accelerate towards the target:
+        accel += (targetPosition - currentPosition) * accelamount;
+
+        //clamp the acceleration so that it doesnt go too fast
+        if (accel > 1) accel = 1;
+        if (accel < -1) accel = -1;
+
+        console.log(targetPosition, currentPosition, accel);
+
+        //move the video scroll position according to the acceleration and how much bouncing you selected:
+        currentPosition = (currentPosition + accel) * (bounceamount) + (targetPosition * (1 - bounceamount));
+
+        //update video playback
+        videoElement[0].currentTime = (currentPosition * pixelDuration) / 1000;
+        videoElement[0].pause();
+
+    }, 80);
 
 });
+
+/*
+ * version 1
+
+ $(function () {
+ var videoWrapper = $('.videoWrapper');
+ var videoElement = videoWrapper.find('video');
+ var frameScrollSize = 2;
+
+ handleScroll = _.throttle(function (newLocation) {
+ console.log('handling scroll');
+ videoElement[0].currentTime = (newLocation * frameScrollSize) / 1000;
+ }, 90, {trailing: false});
+
+ videoElement.on('loadedmetadata', function (event) {
+ console.log(this);
+
+ // adding frameScroll size to compensate for scroll size
+ var videoDuration = (this.duration + frameScrollSize) * 1000;
+ videoWrapper.height(videoDuration / frameScrollSize);
+ });
+ $(window).scroll(function (event) {
+ //console.log($(this).height());
+ handleScroll(this.scrollY);
+ });
+
+
+
+ // testing supported intervals
+ var section = window.location.hash.split('#')[1] || 100;
+ setInterval(function () {
+ video[0].currentTime += section / 1000;
+ }, section)
+
+ });
+ */
+
